5af74dcf24cfe00f7b56d96f0bfcc8e566db77eb
jacob-hanbeen-kim <kjkjkjhan424@gmail.com>
2026-05-16 18:05:52 -0400

    import fonts

---
+++ src/fonts.ts
@@ -0,0 +1,9 @@
+import { loadFont as loadJua } from '@remotion/google-fonts/Jua';
+import { loadFont as loadBlackHanSans } from '@remotion/google-fonts/BlackHanSans';
+import { loadFont as loadGowunDodum } from '@remotion/google-fonts/GowunDodum';
+import { loadFont as loadInter } from '@remotion/google-fonts/Inter';
+
+loadJua();
+loadBlackHanSans();
+loadGowunDodum();
+loadInter();
